178533845508e996ac5488cf63de40972b0d76f8
Alexander Ortiz <alexander.ortiz@adu.edu>
2020-02-19 22:19:38 -0500

    TripsAndUsers in mySql

--- database/mysql/TripsAndUsers.sql
+++ database/mysql/TripsAndUsers.sql
@@ -16,7 +16,8 @@ ENUM type of (‘completed’, ‘cancelled_by_driver’, ‘cancelled_by_client
 | 9  |     3     |    10     |    12   |     completed      |2013-10-03| 
 | 10 |     4     |    13     |    12   | cancelled_by_driver|2013-10-03|
 +----+-----------+-----------+---------+--------------------+----------+
-The Users table holds all users. Each user has an unique Users_Id, and Role is an ENUM type of (‘client’, ‘driver’, ‘partner’).
+The Users table holds all users. Each user has an unique Users_Id,
+ and Role is an ENUM type of (‘client’, ‘driver’, ‘partner’).
 
 +----------+--------+--------+
 | Users_Id | Banned |  Role  |
@@ -30,7 +31,10 @@ The Users table holds all users. Each user has an unique Users_Id, and Role is a
 |    12    |   No   | driver |
 |    13    |   No   | driver |
 +----------+--------+--------+
-Write a SQL query to find the cancellation rate of requests made by unbanned users (both client and driver must be unbanned) between Oct 1, 2013 and Oct 3, 2013. The cancellation rate is computed by dividing the number of canceled (by client or driver) requests made by unbanned users by the total number of requests made by unbanned users.
+Write a SQL query to find the cancellation rate of requests made by unbanned users
+ (both client and driver must be unbanned) between Oct 1, 2013 and Oct 3, 2013.
+ The cancellation rate is computed by dividing the number of canceled (by client or driver)
+ requests made by unbanned users by the total number of requests made by unbanned users.
 
 For the above tables, your SQL query should return the following rows with the cancellation rate being rounded to two decimal places.
 
@@ -66,3 +70,21 @@ insert into Users (Users_Id, Banned, Role) values ('10', 'No', 'driver');
 insert into Users (Users_Id, Banned, Role) values ('11', 'No', 'driver');
 insert into Users (Users_Id, Banned, Role) values ('12', 'No', 'driver');
 insert into Users (Users_Id, Banned, Role) values ('13', 'No', 'driver');
+
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ( "1", "1", "10", "1", "cancelled_by_client", "2013-10-01");
+insert into Users(Users_Id, Banned, Role) values ("1", "No", "client");
+insert into Users(Users_Id, Banned, Role) values ("10", "No", "driver");
+
+/*
+find the unbanned users.
+find the rides that happened between Oct 1, 2013 and Oct 3, 2013.
+*/
+
+# Write your MySQL query statement below
+
+SELECT Trips.Request_at AS 'Day', CAST(SUM(CASE WHEN Status <> 'completed' THEN 1 ELSE 0 END) / Count(Request_at) AS Decimal(3,2)) AS 'Cancellation Rate'
+FROM Users
+INNER JOIN Trips ON Users.Users_Id = Trips.Client_Id and Banned <> 'Yes'
+WHERE Request_at BETWEEN '2013-10-01' and '2013-10-03'
+And Users.Banned <> 'Yes'
+GROUP BY Trips.Request_at
